b80c3564a2f678e0a9f61fd084ae2804cb013e62
lev2099 <lev2099@gmail.com>
2024-11-13 21:28:25 +0200

    switch form class

--- taxi/views.py
+++ taxi/views.py
@@ -107,7 +107,7 @@ class DriverCreateView(LoginRequiredMixin, generic.CreateView):
 
 class DriverUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Driver
-    form_class = DriverCreateForm
+    form_class = DriverLicenseUpdateForm
 
 
 class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
